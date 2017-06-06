d2521f55f853e17483519eb25cdaa301f514c73a
jparent88 <jparent88@gmail.com>
2017-06-06 15:58:48 -0400

    Completed Vending Machine Program

---
+++ CapstoneVendingMachineProject/Capstone/Classes/Vendomatic500.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Capstone.Classes
+{
+    public static class Vendomatic500
+    {
+        public static void VendomaticAscii()
+        {
+            string filePath = "Vendomatic500ascii.txt";
+            try
+            {
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string vendomaticAscii = sr.ReadLine();
+                        Console.WriteLine(vendomaticAscii);
+                    }
+                }
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Vendomatic500ascii.txt. Please exit the console and copy file from Capstone.etc folder to Captone.bin.Debug folder.");
+                Environment.Exit(0);
+            }
+        }
+    }
+}
